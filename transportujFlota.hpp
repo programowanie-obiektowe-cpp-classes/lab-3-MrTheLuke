7a8a56ae5fa7f2674ad11f15075869273f1208c5
Łukasz Spasiński <01171680@pw.edu.pl>
2024-11-10 14:34:52 +0100

    zejściówka laby 3

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -1,9 +1,28 @@
 #pragma once
-
-#include "Stocznia.hpp"
+#include <iostream>
+#include "Stocznia.hpp" // Zak³adamy, ¿e wszystkie pliki s¹ zaimportowane w odpowiednim porz¹dku
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0)
+        return 0; // Nie tworzymy statków, jeœli nie ma towaru
+
+    Stocznia     stocznia;               // Tworzymy obiekt stoczni
+    unsigned int przetransportowany = 0; // Ca³kowita iloœæ przetransportowanego towaru
+
+    CountThis< Zaglowiec >::reset();
+
+    // Pêtla dopóki nie przetransportujemy wystarczaj¹cej iloœci towaru
+    while (przetransportowany < towar) {
+
+        Statek* statek = stocznia();
+
+        // Transportujemy towar
+        przetransportowany += statek->transportuj();
+
+        delete statek;
+    }
+
+    // Zwracamy liczbê ¿aglowców u¿ytych do transportu
+    return CountThis< Zaglowiec >::get();
 }
